f4314e36b4a55017190acd805318b4bf2c864547
Jose Alves <jsalves@thoughtworks.com>
2019-01-03 21:58:33 -0300

    MM-0000 [Jose] Initial structure

---
+++ MyMovies/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyMovies
+//
+//  Created by Jose Alves on 03/01/19.
+//  Copyright © 2019 Vision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
